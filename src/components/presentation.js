749bf3027301e189c6bf422e46fcd3a9b93c6e4d
Avinash Madavan <madavan2@illinois.edu>
2020-01-26 13:06:00 -0600

    Add presentations

--- src/components/presentation.js
+++ src/components/presentation.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-export default () => (
-  <span>
+const presentations = require('../../db/presentations');
 
-  </span>
+class Presentation extends React.Component {
+  render() {
+    const {data} = this.props;
+    return (
+      <li>
+        {data.title && <span>{data.title}. </span>}
+        {data.presented_at && <span>Presented at: <i>{data.presented_at}</i> </span>}
+        {data.url && <span>[<a href={data.url}>file</a>]</span>}
+      </li>
+    );
+  }
+}
+
+export default () => (
+  <ul>
+    {presentations.map((pres) => <Presentation data={pres}/>)}
+  </ul>
 );
